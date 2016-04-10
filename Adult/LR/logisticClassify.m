8642cad0f28385d8c60d61670bc8db86f17644e1
jonathanlxy <jonathanlxy@gmail.com>
2016-04-10 10:41:33 -0400

    Logistic Regression

--- Adult/LR/logisticClassify.m
+++
@@ -1,5 +0,0 @@
-function labelVector = logisticClassify(x, w)
-    labelVector = zeros(size(x, 1), 1);
-    for i = 1:size(x, 1)
-        labelVector(i) = sigmoidLikehood(x(i,:), 1, w) > sigmoidLikehood(x(i,:), 0, w);
-    end
--- Adult/LR/sigmoidLikehood.m
+++
@@ -1,6 +0,0 @@
-function L = sigmoidLikehood(x, y, w)
-% x: features of a data point
-% y: single number 
-sig = 1/(1 + exp(-dot(w,x)));
-L = ( (1 - sig)^(1 - y) ) * ( sig^y );
-    
